aee38dbfd52cd237f6ef3aef40ca6e58a64cd43f
Rushantha Wijenayake <rush@gayan.com.au>
2023-09-14 03:17:23 +0000

    Implemented option handling and help message trigger

--- user_upload.php
+++ user_upload.php
@@ -1,2 +1,12 @@
+<?php
 // Command line directives
 $options = getopt("u:p:h:d:", ["file:", "create_table", "dry_run", "u:", "p:", "h:", "d:", "help"]);
+
+if (isset($options['help'])) {
+    displayHelpMessage();
+    exit();
+}
+
+if (!isset($options['u']) || !isset($options['p']) || !isset($options['h']) || !isset($options['d'])) {
+    die("Please provide u, p, h, and d options. Type --help for more details.\n"); 
+}
